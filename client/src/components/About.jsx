95f0a477de1e2537f63d94e9879f1962892177a3
Andrew Hollingworth <andrew.hollingworth@gmail.com>
2019-10-24 19:37:23 -0400

    Fix Typo in 'About' component

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
         Hey, I'm Andrew.
       </Typography>
       <Typography className={classes.body} variant='body1'>
-        I’m a web developer with a passion for making things, storytelling, and learning. My work in production, taught me to plan my code three steps ahead and stay organized. My passion for storytelling helps me articulate my ideas to others on my team accurately and effectively. And my love of learning means I’m always looking for new challenges.
+        I’m a web developer with a passion for making things, storytelling, and learning. My work in production taught me to plan my code three steps ahead and stay organized. My passion for storytelling helps me articulate my ideas to others on my team accurately and effectively. And my love of learning means I’m always looking for new challenges.
       </Typography>
       <img className={classes.profile} src="https://i.imgur.com/bRgMl6q.png" alt="My Pretty Face" />
     </div>
